cb898d0b6c21a8b6ae0ca44ada341750d50423d2
Gustavo <gustavo.gigante.s@gmail.com>
2020-03-08 13:49:12 +0000

    Page Template. Follow sketchbook structure from now on!

---
+++ page-templates/test-template.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Test Template
+ *
+ * Template for displaying a page just with the header and footer area and a "naked" content area in between.
+ * Good for landingpages and other types of pages where you want to add a lot of custom markup.
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+echo "Test Template";
+
+get_footer();
